db25ea49def92d3a8ceab905ba2fc22e042bbe58
LeonardVollmann <vollmann.leonard@gmail.com>
2015-03-27 10:45:14 +0100

    Set up the build system

---
+++ CMakeFiles/Lotus.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lotus.dir/src/main.cpp.o"
+  "Lotus.pdb"
+  "Lotus"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Lotus.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
